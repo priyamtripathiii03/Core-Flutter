0fca544e9260569ec66d8338d2bd5f5d23154871
Priyam Tripathi <149374259+priyamtripathiii03@users.noreply.github.com>
2024-06-10 16:34:49 +0530

    Update q1.dart

--- Lecture 2/q1.dart
+++ Lecture 2/q1.dart
@@ -3,18 +3,12 @@ import 'dart:io';
 void main()
 {
   int a;
-  List l1=[];
-  stdout.write("enter the list size : ");
-   a=int.parse(stdin.readLineSync()!);
+  List l1=[1,2,3];
 
-  for(int i=0;i<a;i++)
-  {
-     stdout.write("enter the element ${[i]} : ");
-    l1.add(int.parse(stdin.readLineSync()!));
-  }
   for(int i=0;i<l1.length;i++)
   {
      l1[i]="${l1[i]}Hello";
   }
   print(l1);
 }
+
