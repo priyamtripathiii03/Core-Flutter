62d893f5634b08c4a3dac616af11174ecf17d56b
Priyam Tripathi <149374259+priyamtripathiii03@users.noreply.github.com>
2024-06-28 14:45:11 +0530

    Update train details.dart

--- Lecture 4.2/train details.dart
+++ Lecture 4.2/train details.dart
@@ -2,8 +2,8 @@ import 'dart:io';
 
 class Railway
 {
-  int? _TrainNumber;
-  String? _TrainName, _Source, _Destignation, _TrainTime;
+   late int _TrainNumber,_n;
+   late String _TrainName, _Source, _Destignation, _TrainTime;
 
   void set()
   {
@@ -21,12 +21,12 @@ class Railway
   }
  void get()
  {
-   stdout.write("Enter the Train Details : \n");
-   stdout.write("Enter the Train Number : $_TrainNumber \n");
-   stdout.write("Enter the Train Name : $_TrainName \n");
-    stdout.write("Enter the Source : $_Source \n");
-     stdout.write("Enter the Destignation : $_Destignation \n");
-      stdout.write("Enter the Train Time : $_TrainTime \n");
+   stdout.write("Train Details : \n");
+   stdout.write("Train Number : $_TrainNumber \n");
+   stdout.write("Train Name : $_TrainName \n");
+    stdout.write("Source : $_Source \n");
+     stdout.write("Destignation : $_Destignation \n");
+      stdout.write("Train Time : $_TrainTime \n");
  }
  void choice()
  {
@@ -34,29 +34,52 @@ class Railway
   stdout.write("Enter 0 for Exit \n");
   stdout.write("Enter 1 to find the Train Number \n");
   stdout.write("Enter 2 to find the Train Name \n");
-  stdout.write("Enter 3 to find the source \n");
-  stdout.write("Enter 4 to find the Destignation \n");
-  stdout.write("Enter 5 to find the Train Time \n");
   stdout.write("Enter your Choice \n");
   int n=int.parse(stdin.readLineSync()!);
 
 
  }
 }
-int main()
+void main()
 {
-  Railway r1=Railway();
-   stdout.write("Enter the Train Number \n");
-   int a=int.parse(stdin.readLineSync()!);
    List<Railway> i1=[];
-
-   for(int i=0;i<a;i++)
+   Railway r1=Railway();
+   for(int i=0;i<3;i++)
    {
     Railway r1=Railway();
     r1.set();
-    r1.get();
+    i1.add(r1);
    }
+   do
+   {
+    for(int i=0;i<i1.length;i++)
+    {
+      stdout.write("Train number : ${i1[i]._TrainNumber}\n");
+    }
+    r1.choice();
+    switch(r1._n)
+    {
+      case 1: for(int i=0;i<i1.length;i++)
+      {
+        i1[i].get();
+      } 
+      break;
+      case 2: stdout.write("Enter the Train Number : \n");
+      int a=int.parse(stdin.readLineSync()!);
+      for(int i=0;i<i1.length;i++)
+      {
+        if(a==i1[i]._TrainNumber)
+        {
+          i1[i].get();
+          break;
+          a=0;
+        }
+      }
+      break;
+      case 0: print("Exit");
 
-
+    }
+   }
+   while(r1._n!=0);
 
 }
